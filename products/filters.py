271d52e4a611153f75077fd89db2f7e408c97df6
Berdishukur <boboqulovberdishukur@gmail.com>
2023-12-12 20:36:06 +0500

    add

--- products/filters.py
+++ products/filters.py
@@ -4,3 +4,7 @@ from .models import Product
 class ProductFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name="price", lookup_expr='lte')
+
+    class Meta:
+        model = Product
+        fields = ['category', 'min_price', 'max_price']
