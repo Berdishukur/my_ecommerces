1fda98144c9cbd0fe6c6ae0bae180bf90b2b4422
Berdishukur <boboqulovberdishukur@gmail.com>
2023-12-16 08:33:35 +0500

    add

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,17 @@ from django.urls import path, re_path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="E-commerce API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
