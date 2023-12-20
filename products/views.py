7c6b48c71ecd7d1aaddc8449c22532c53569b175
Berdishukur <boboqulovberdishukur@gmail.com>
2023-12-20 14:34:17 +0500

    add

---
+++ products/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.db import models
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from .filters import ProductFilter,ReviewFilter
+from django_filters import rest_framework as django_filters
+
+from products.models import Product, Review, Category
+from products.serializers import ProductSerializer, ReviewSerializer, CategorySerializer
+from rest_framework import generics,filters
+from rest_framework.permissions import IsAuthenticated
+
+class CustomPagination(PageNumberPagination):
+    page_size = 3
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    pagination_class = CustomPagination
+
+    filter_backends = (django_filters.DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ['rating']
+    filterset_class=ReviewFilter
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    pagination_class = CustomPagination
+
+    filter_backends = (django_filters.DjangoFilterBackend,filters.SearchFilter)
+    filterset_class=ProductFilter
+    search_fields=['name','description']
+
+
+    def list(self, request, *args, **kwargs):
+        category = request.query_params.get('category', None)
+        if category:
+            self.queryset = self.queryset.filter(category=category)
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        related_products = Product.objects.filter(category=instance.category).exclude(id=instance.id)[:5]
+        related_serializer = ProductSerializer(related_products, many=True)
+        return Response({
+            'product': serializer.data,
+            'related_products': related_serializer.data
+        })
+
+    @action(detail=False, methods=['get'])
+    def top_rated(self, request):
+        # Assuming a related name of "reviews" from Review model to Product model
+        top_products = Product.objects.annotate(avg_rating=models.Avg('reviews__rating')).order_by('-avg_rating')[:2]
+        serializer = ProductSerializer(top_products, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def average_rating(self, request, pk=None):
+        product = self.get_object()
+        reviews = product.reviews.all()
+
+        if reviews.count() == 0:
+            return Response({"average_rating": "No reviews yet!"})
+
+        avg_rating = sum([review.rating for review in reviews]) / reviews.count()
+
+        return Response({"average_rating": avg_rating})
