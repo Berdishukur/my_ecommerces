12ff024412d367420f2b8a48632a9b410389a5d1
Berdishukur <boboqulovberdishukur@gmail.com>
2023-12-23 17:29:15 +0500

    add

--- products/services/product_view_history.py
+++ products/services/product_view_history.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from products.models.product import ProductViewHistory
+from products.models import ProductViewHistory
 from products.serializers import ProductViewHistorySerializer
 from drf_yasg.utils import swagger_auto_schema
 
