a92a9898844dd71f574026158dd0fa7c9c67611f
Berdishukur <boboqulovberdishukur@gmail.com>
2024-01-05 16:28:06 +0500

    add

--- products/services/product_view_history.py
+++ products/services/product_view_history.py
@@ -5,13 +5,3 @@ from products.models import ProductViewHistory
 from products.serializers import ProductViewHistorySerializer
 from drf_yasg.utils import swagger_auto_schema
 
-class ProductViewHistoryCreate(APIView):
-    serializer_class = ProductViewHistorySerializer
-
-    @swagger_auto_schema(request_body=ProductViewHistorySerializer)
-    def post(self, request):
-        serializer = ProductViewHistorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
